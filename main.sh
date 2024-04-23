2e43fa27fa50be8b41151d4c32cd90696aaa2486
Grant Smith <gsmith12@luc.edu>
2024-04-22 20:40:41 -0500

    second commit

--- main.sh
+++ main.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
 #Extra-Credit Assignment
 
+#define output directory
+output_directory="output"
 
+#check if output directory exists before making another one
+if [! -d "$output_directory" ]; then
+	mkdir -p "$output_directory"
+	echo "output directory created."
+fi
+
+read 
